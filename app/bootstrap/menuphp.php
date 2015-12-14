7f572e9af0f1f3335a99312a5ffd866314b5ed61
Javi <javierlopez1@iesebre.com>
2015-12-14 16:39:28 +0100

    Cambiem el nom de la app utilitzant php srtisan app:name

---
+++ app/bootstrap/menuphp.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: javi
+ * Date: 14/12/15
+ * Time: 16:29
+ */
+//Utilitxem MenuWithauthentication com a namespace per a que no cee conflictes.
+//Home
+MenuWithAuthentication::menu
+
+//Other Link
+MenuWithAuthentication::menu
+
+//Multilevel
+MenuWithAuthentication::menu
+    MenuWithAuthentication::menu
+    MenuWithAuthentication::menu
