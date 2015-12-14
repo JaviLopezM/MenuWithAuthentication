60bcc864b7dfd7ffc1fa6eba14b1493b3e9c48f0
Javi <javierlopez1@iesebre.com>
2015-12-14 16:55:54 +0100

    modifico l'apariencia del menú

--- app/bootstrap/menuphp.php
+++ app/bootstrap/menuphp.php
@@ -7,12 +7,28 @@
  */
 //Utilitxem MenuWithauthentication com a namespace per a que no cee conflictes.
 //Home
-MenuWithAuthentication::menu
+MenuWithAuthentication::menu()
+    ->title("Home")
+    ->icon('fa-dashboard')
+    ->url('home')
+    ->rol('home')
+    ->permission('home');
 
 //Other Link
-MenuWithAuthentication::menu
+MenuWithAuthentication::menu()
+    ->title("Another Link");
 
 //Multilevel
-MenuWithAuthentication::menu
-    MenuWithAuthentication::menu
-    MenuWithAuthentication::menu
+MenuWithAuthentication::menu()
+    ->title("Multilevel")->icon('fa-credit-card');
+    MenuWithAuthentication::menu()
+        ->title("Link in Level 1")
+        ->icon('fa-briefcase');
+    MenuWithAuthentication::menu()
+        ->title("Link in level 2")
+        ->icon('fa-user');
+
+MenuWithAuthentication::menu()
+    ->title("Últim menú")
+    ->icon('fa-dashboard')
+    ->url('http://www.google.com');
